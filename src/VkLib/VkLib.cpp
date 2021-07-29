5b47f961bb017c0d5167d2ff131b70a88d4f1df2
Pansystar <wh.bt3.p2@gmail.com>
2021-07-29 09:11:47 +0900

    Update VkLib_Init method.

--- src/VkLib/VkLib.cpp
+++ src/VkLib/VkLib.cpp
@@ -1,9 +1,44 @@
-#include "VkLib.h"
+#include "vulkan/vulkan.h"
+#include "vulkan/vulkan_core.h"
+
+#include <vector>
 
 namespace VkLib {
     int32_t VkLib_Init() {
-        
-        return VK_SUCCESS;
+        VkResult result;
+
+        VkApplicationInfo aInfo;
+        aInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        aInfo.pNext = nullptr;
+        aInfo.pApplicationName = "VkLib";
+        aInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        aInfo.pEngineName = "No Engine";
+        aInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        aInfo.apiVersion = VK_MAKE_VERSION(1, 0, 0);
+
+        VkInstanceCreateInfo cinfo;
+        cinfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        cinfo.pNext = nullptr;
+        cinfo.flags = 0;
+        cinfo.pApplicationInfo = &aInfo;
+
+        VkInstance instance;
+
+        result = vkCreateInstance(&cinfo, nullptr, &instance);
+
+        if(result != VK_SUCCESS) {
+            return -1;
+        }
+
+        uint32_t physicalDeviceArraySize;
+        vkEnumeratePhysicalDevices(instance, &physicalDeviceArraySize, nullptr);
+
+        std::vector<VkPhysicalDevice> physicalDeviceArray(physicalDeviceArraySize);        
+        vkEnumeratePhysicalDevices(instance, &physicalDeviceArraySize, physicalDeviceArray.data());
+
+        return 0;
+    }
+
     int32_t ProcessMessage() {
         return 0;
     }
