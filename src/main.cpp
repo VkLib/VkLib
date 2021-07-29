7d2fafeff213065454b00fcc86078b76f97b8c85
Pansystar <wh.bt3.p2@gmail.com>
2021-07-29 09:07:47 +0900

    Remake pre-environment

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <stdio.h>
+// #include "VkLib/VkLib.h"
+
+// using namespace VkLib;
 
 int main(void) {
-    printf("Hello, world.");
+    // VkLib_Init();
+
+    // while(ProcessMessage() == 0)
+    // {
+    //     break;
+    // }
+
+    // VkLib_End();
     return 0;
+    
 }
