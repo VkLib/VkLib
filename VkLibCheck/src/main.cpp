4cd775d0d79a6d5e4c47db2c5de8e44a60ca1224
Pansystar <wh.bt3.p2@gmail.com>
2021-08-03 23:22:14 +0900

    Update main.cpp

--- VkLibCheck/src/main.cpp
+++ VkLibCheck/src/main.cpp
@@ -4,25 +4,19 @@ using namespace VkLib;
 
 #include <stdio.h>
 
+#define str(val) #val
+
 int main(void) {
     int32_t ret = 0;
 
     ret = VkLib_Init();
 
     if(ret != 0) {
-        switch (ret)
-        {
-        case -2:
-            printf("Vulkanに対応していないGPUを積んだPCで使用されています。\n");
-            break;        
-        default:
-            break;
-        }
-        return -1;
+        printf("%s(): %d", str(VkLib_Init),ret);
     }
 
     while(ProcessMessage() == 0) {
-        printf("oh\n");
+        
     }
 
     VkLib_End();
