9e756382afab3b834a87cba1c31952a0489e4416
Pansystar <wh.bt3.p2@gmail.com>
2021-08-03 23:22:39 +0900

    VkLib init glfw

--- VkLib/src/VkLib.cpp
+++ VkLib/src/VkLib.cpp
@@ -4,63 +4,80 @@
 #include <vulkan/vulkan.h>
 #include <vulkan/vulkan_core.h>
 
+#include <GLFW/glfw3.h>
+
 #include <vector>
 #include <cstdio>
+
+namespace {
+    GLFWwindow* window;
+}
 
 namespace VkLib {
     int32_t VkLib_Init() {
         
-        VkResult result;
+        glfwInit();
 
-        VkApplicationInfo aInfo;
-        aInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-        aInfo.pNext = nullptr;
-        aInfo.pApplicationName = "VkLib";
-        aInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-        aInfo.pEngineName = "No Engine";
-        aInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-        aInfo.apiVersion = VK_MAKE_VERSION(1, 0, 0);
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        window = glfwCreateWindow(960, 540, "VkLib", nullptr, nullptr);
 
-        VkInstanceCreateInfo cinfo;
-        cinfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-        cinfo.pNext = nullptr;
-        cinfo.flags = 0;
-        cinfo.pApplicationInfo = &aInfo;
 
-        VkInstance instance;
+        // VkResult result;
 
-        result = vkCreateInstance(&cinfo, nullptr, &instance);
+        // VkApplicationInfo aInfo;
+        // aInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        // aInfo.pNext = nullptr;
+        // aInfo.pApplicationName = "VkLib";
+        // aInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        // aInfo.pEngineName = "No Engine";
+        // aInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        // aInfo.apiVersion = VK_MAKE_VERSION(1, 0, 0);
 
-        if(result != VK_SUCCESS) {
-            switch(result) {
-                case VK_ERROR_EXTENSION_NOT_PRESENT:
-                {
-                    return -2;
-                }
-                break;
-                default:
-                {
-                    return -1;
-                }
-                break;
-            }
-        }
+        // VkInstanceCreateInfo cinfo;
+        // cinfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        // cinfo.pNext = nullptr;
+        // cinfo.flags = 0;
+        // cinfo.pApplicationInfo = &aInfo;
 
-        uint32_t physicalDeviceArraySize;
-        vkEnumeratePhysicalDevices(instance, &physicalDeviceArraySize, nullptr);
+        // VkInstance instance;
 
-        std::vector<VkPhysicalDevice> physicalDeviceArray(physicalDeviceArraySize);        
-        vkEnumeratePhysicalDevices(instance, &physicalDeviceArraySize, physicalDeviceArray.data());
+        // result = vkCreateInstance(&cinfo, nullptr, &instance);
+
+        // if(result != VK_SUCCESS) {
+        //     switch(result) {
+        //         case VK_ERROR_EXTENSION_NOT_PRESENT:
+        //         {
+        //             return -2;
+        //         }
+        //         break;
+        //         default:
+        //         {
+        //             return -1;
+        //         }
+        //         break;
+        //     }
+        // }
+
+        // uint32_t physicalDeviceArraySize;
+        // vkEnumeratePhysicalDevices(instance, &physicalDeviceArraySize, nullptr);
+
+        // std::vector<VkPhysicalDevice> physicalDeviceArray(physicalDeviceArraySize);        
+        // vkEnumeratePhysicalDevices(instance, &physicalDeviceArraySize, physicalDeviceArray.data());
 
         return 0;
     }
 
     int32_t ProcessMessage() {
-        return -1;
+
+        glfwPollEvents();
+
+        return glfwWindowShouldClose(window);
     }
 
     int32_t VkLib_End() {
-        
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
         return 0;
     }
 }
