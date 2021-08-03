acd5e54b135f3df0f221e26dcd7a38dd4528aa48
Pansystar <wh.bt3.p2@gmail.com>
2021-08-03 23:22:26 +0900

    Update batch build.

--- batch/Compile-VkLibCheck_x86-Debug.bat
+++ batch/Compile-VkLibCheck_x86-Debug.bat
@@ -1,8 +1,18 @@
 chcp 65001
 
+set FILENAME_VKLIB=VkLib.dll
+set FILENAME_LIBSTDCPP=libstdc++-6.dll
+set FILENAME_LIBSTDC=libstdc_s_dw2-1.dll
+set FILENAME_GLFW=glfw3.dll
+
+set FOLDER_OUTPUT=VkLibCheck/bin/x86/Debug
+
 cd ..
 mkdir "VkLibCheck/bin/x86/Debug"
-copy ".\VkLib\bin\x86\Debug\VkLib.dll" "VkLibCheck/bin/x86/Debug/VkLib.dll"
+copy ".\VkLib\bin\x86\Debug\%FILENAME_VKLIB%" "%FOLDER_OUTPUT%/%FILENAME_VKLIB%"
+copy "C:\MinGW\bin\%FILENAME_LIBSTDCPP%" "%FOLDER_OUTPUT%/%FILENAME_LIBSTDCPP%"
+copy "C:\MinGW\bin\%FILENAME_LIBSTDC%" "%FOLDER_OUTPUT%/%FILENAME_LIBSTDC%"
+copy "C:\GLFW\glfw-3.3.4\lib-mingw\%FILENAME_GLFW%" "%FOLDER_OUTPUT%/%FILENAME_GLFW%"
 
 C:/MinGW/Bin/g++.exe VkLibCheck/src/** ^
 -g ^
--- batch/Compile-VkLib_x86-Debug.bat
+++ batch/Compile-VkLib_x86-Debug.bat
@@ -9,9 +9,12 @@ C:/MinGW/Bin/g++.exe VkLib/src/** ^
 -g ^
 -IC:/VulkanSDK/%VULKAN_VERSION%/Include/ ^
 -LC:/VulkanSDK/%VULKAN_VERSION%/Lib32/ ^
+-IC:/GLFW/glfw-3.3.4/include/ ^
+-LC:/GLFW/glfw-3.3.4/lib-mingw/ ^
 -o ^
 VkLib/bin/x86/Debug/VkLib.dll ^
 -lvulkan-1 ^
+-lglfw3dll ^
 -shared ^
 "-Wl,--out-implib,VkLib/bin/x86/Debug/VkLib.lib"
 
