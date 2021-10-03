43079cd8d31c50dd887bdee1f4b184e6d389c973
Pansystar <wh.bt3.p2@gmail.com>
2021-10-04 00:54:59 +0900

    仮処理を追加

--- VkLib/src/VkGraphics.cpp
+++ VkLib/src/VkGraphics.cpp
@@ -1,17 +1,18 @@
-#include "VkGraphics.h"
+#include "VkLib.h"
 
+#include <filesystem>
 
-#define VK_SUCCESS 0
+int32_t LoadGraph(const TCHAR* pFilePath) {
+    constexpr int RESULT_LOAD_FAILED = -1;
+    namespace fs = std::filesystem;
 
-class VkGraphics {
-public:
+    if (!fs::exists(pFilePath)) {
+        return LOAD_FAILED;
+    }
 
-};
-
-int32_t LoadGraph(const uint8_t* pFilePath) {
-    return VK_SUCCESS;
+    return TRUE;
 }
 
-int32_t DrawGraph(int x, int y, int handle) {
-    return VK_SUCCESS;
+int32_t DrawGraph(int32_t x, int32_t y, int32_t handle) {
+    return TRUE;
 }
